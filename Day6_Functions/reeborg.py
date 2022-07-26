b53ef429a40c87995fa07e10f09884c449fd1535
Ali Chattha <ali@Alis-MBP.attlocal.net>
2022-07-25 22:27:54 -0500

    reeborg

---
+++ Day6_Functions/reeborg.py
@@ -0,0 +1,27 @@
+def turn_around():
+    turn_left()
+    turn_left()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+move()
+move()
+turn_left()
+move()
+turn_right()
+move()
+
+def draw_square():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    
+draw_square()
