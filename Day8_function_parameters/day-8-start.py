5337a4c9edd1fdee544360499a9d30189edf8623
Ali Chattha <ali@Alis-MBP.attlocal.net>
2022-08-15 08:41:54 -0500

    day8.1

--- Day8_function_parameters/day-8-start.py
+++ Day8_function_parameters/day-8-start.py
@@ -1,9 +1,28 @@
+# Functions with more than a single input or multiple inputs
+
+# Using Keyword Arguments. Less error prone
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What's it like in {location}?")
+
+greet_with(name = "Ali", location = "Franklin")
+
+
 # Functions with inputs
+#                 (parameter)
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How are you {name}?")
+    
+# #              (argument)
+# greet_with_name("Ali")
 
-def greet():
-    print("Greetings")
-    print("Hello")
-    print("World")
 
 
-greet()
+# Simple function
+
+# def greet():
+#     print("Greetings")
+#     print("Hello")
+#     print("World")
+# greet()
