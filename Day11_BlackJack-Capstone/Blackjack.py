cab8651820b8b2be90f504efd2c658fe306f0b8d
Ali <alishanc31@gmail.com>
2022-10-06 22:42:54 -0500

    more catchup

--- Day11_BlackJack-Capstone/Blackjack.py
+++ Day11_BlackJack-Capstone/Blackjack.py
@@ -3,6 +3,8 @@ import random
 from art import logo
 print(logo)
 
+""" Refer to Blackjack_Solution.py for Hints as this is meant to be clean."""
+
 ############### Our Blackjack House Rules #####################
 
 """The deck is unlimited in size.
@@ -16,6 +18,44 @@ Cards are not removed from the deck as they are drawn.
 The computer is the dealer."""
 
 ##################### Hints #####################
+
+
+def deal_card():
+    """Returns a random card from the deck."""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+
+
+user_cards = []
+computer_cards = []
+game_over = False
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    game_over = True
+else:
+    user_should_deal = input(
+        "Type 'y' to get another card. Type 'n' to pass: ")
+    if user_should_deal.lower() == "y":
+        user_cards.append(deal_card())
+    else:
+        game_over = True
+
+
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
 
 # Hint 1: Go to this website and try out the Blackjack game:
 #   https://games.washingtonpost.com/games/blackjack/
@@ -33,40 +73,15 @@ The computer is the dealer."""
 # 11 is the Ace.
 # cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-
-def deal_card():
-    """Returns a random card from the deck."""
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    card = random.choice(cards)
-    return card
-
-
 # Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-user_cards = []
-computer_cards = []
 
-for _ in range(2):
-    # new_card = deal_card() - redundant so add deal_card() directly into append function below
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
+ # Hint 6: Create a function called calculate_score() that takes a List of cards as input
+    # and returns the score.
+    # Look up the sum() function to help you do this.
 
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
+# Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
 
-# Hint 6: Create a function called calculate_score() that takes a List of cards as input
-# and returns the score.
-# Look up the sum() function to help you do this.
-
-
-def calculate_score(cards):
-    # Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
-    if sum(cards) == 21 and len(cards) == 2:
-        return 0
 # Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
-    if 11 in cards and sum(cards) > 21:
-        cards.remove(11)
-        cards.append(1)
-    return sum(cards)
 
 # Hint 9: Call calculate_score(). If the computer or the user has a blackjack(0) or if the user's score is over 21, then the game ends.
 
