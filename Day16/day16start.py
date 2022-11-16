df198b47be28c6c8a8b9f5003cf94fb842e7e9f3
Ali <alishanc31@gmail.com>
2022-11-16 14:23:55 -0600

    day 16

---
+++ Day16/day16start.py
@@ -0,0 +1,36 @@
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Aazam", "Ali", "Mom"])
+table.add_column("Type", ["Water", "Electric", "Wind"])
+
+table.align = "l"
+
+print(table)
+
+# importing module and assigning class
+# import turtle
+# timmy = turtle.Turtle()
+
+# # import the module from the class and assign to object
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# def square():
+#     global timmy
+#     timmy.shape("turtle")
+#     timmy.color("coral")
+#     timmy.forward(100)
+#     timmy.right(90)
+#     timmy.forward(100)
+#     timmy.right(90)
+#     timmy.forward(100)
+#     timmy.right(90)
+#     timmy.forward(100)
+# square()
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+
