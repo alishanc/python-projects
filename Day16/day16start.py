b11f091a26e2b667ca291bdf0cba06f5dffa1859
Ali <alishanc31@gmail.com>
2023-01-10 12:32:41 -0600

    day 16 turtle update

--- Day16/day16start.py
+++ Day16/day16start.py
@@ -8,29 +8,29 @@ table.align = "l"
 print(table)
 
 # importing module and assigning class
-# import turtle
-# timmy = turtle.Turtle()
+import turtle
+timmy = turtle.Turtle()
 
-# # import the module from the class and assign to object
-# from turtle import Turtle, Screen
-# timmy = Turtle()
-# print(timmy)
-# def square():
-#     global timmy
-#     timmy.shape("turtle")
-#     timmy.color("coral")
-#     timmy.forward(100)
-#     timmy.right(90)
-#     timmy.forward(100)
-#     timmy.right(90)
-#     timmy.forward(100)
-#     timmy.right(90)
-#     timmy.forward(100)
-# square()
-#
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+# import the module from the class and assign to object
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+def square():
+    global timmy
+    timmy.shape("turtle")
+    timmy.color("coral")
+    timmy.forward(100)
+    timmy.right(90)
+    timmy.forward(100)
+    timmy.right(90)
+    timmy.forward(100)
+    timmy.right(90)
+    timmy.forward(100)
+square()
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
 
 
 
